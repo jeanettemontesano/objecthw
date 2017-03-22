0b321c297810267bde96043f0cb061ce0568a338
jeanette montesano <escapetheearth@gmail.com>
2017-03-21 21:05:27 -0400

    objects3

--- objecthw.js
+++ objecthw.js
@@ -4,6 +4,15 @@ function Photo(filepath,location){
   this.location= location;
 }
 
+function Album (){
+	this.photos = ();
+}
+
+album.prototype.addpic = function (photo){
+	this.photos.push(photo)
+}
+
+var album = newAlbum()album.addPic
 
 var newPhoto= new photo("flower.jpeg","flower","rose")
 var newPhoto2= new photo ("scottland.jpeg","scottland", "bar")
